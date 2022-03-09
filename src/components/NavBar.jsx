5d8c5eac86bff584fa4de75f9326b687bdded1e1
Nlakaniplo Khumalo <nlakaniplo.khumalo@dsktech.co.za>
2022-03-09 12:24:19 +0200

    Icons installed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import {BiLogIn} from "react-icons/bi";
+import {TiArrowShuffle} from "react-icons/ti";
 
 export default function NavBar() {
   return (
     <header className="app-header">
       <div className="top-nav">
-        <h2 className="logo">FlexMind</h2>
-        <h4 className="auth">Auth</h4>
+        <h2 className="logo"><span>Flex</span>Mind<i className="nav-icon"><TiArrowShuffle/></i></h2>
+       
+        <form className="searchBox">
+            <input className="searchInput"type="text" name="" placeholder="Search"/>
+            <button className="searchButton" href="#">
+                <i className="material-icons">
+                    search
+                </i>
+            </button>
+        </form>
+       
+        <h4 className="auth"><BiLogIn/></h4>
       </div>
       <div className="main-nav">
         <NavLink className={"nav-link"} to='/'>Home</NavLink>
